7c430522185eae55da0d65e75fed80c503eeffde
hanukoon <hwymaster01@gmail.com>
2019-03-10 23:15:31 +0900

    Update thanos.py

--- thanos.py
+++ thanos.py
@@ -3,21 +3,21 @@
 import os
 import sys
 import random
-# arg1 = sys.argv[1]
-# arg2 = sys.argv[2]
+arg1 = sys.argv[1]
+
 b = 0
 i = 0
 
 filelist = []
-target = os.getcwd()
+deletelist = []
+target = os.getcwd() 
+target += arg1
+print("[😈] Target is" + target + " !")
 
-# def getFiles(dir):
-#     x = 0
-#     for pack in os.walk(dir):
-#         #print( " current directory = %s , sub directory = %s  , files = %s " % ( pack[0] , pack[1],pack[2] ) )
-#         for f in pack[2]:
-#             x += 1
-#     return x
+if (arg1 == ""):
+    print("Directory is not selected! plz check and give me like thanos.py /Desktop")
+    print("If u want to remove this dir, plz give me argv like thanos.py /")
+
 def search(dirname):
     try:
         filenames = os.listdir(dirname)
@@ -27,10 +27,21 @@ def search(dirname):
                 search(full_filename)
             filelist.append(full_filename)
     except PermissionError:
-        print("😈 Permission Error. Thanos need Root Perminssion")
+        print("[😈] Permission Error. Thanos need Root Perminssion")
+
 
 # allfilecount = getFiles(target)
+
 # halffilecount = allfilecount / 2
 search(target)
 random.shuffle(filelist)
-print(filelist)
+print("[😈] Death note is ready...")
+filecount = len(filelist)
+for i in range(0, int(filecount / 2)):
+    deletelist.append(filelist[i])
+    print (deletelist[i])
+print("[😈] Snapping fingers inside "+ os.getcwd() +" with full power...")
+for i in range(filecount + 1):
+    os.remove(str(deletelist[i]))
+print("[😈] I'm done, half of files are still exist.")
+
